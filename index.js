c960a69eb29b3338562582b4bdc7703072cd351d
ArthurYuLunHung <arthur0420@gmail.com>
2024-04-29 20:36:23 -0700

    thanks youtube

---
+++ index.js
@@ -0,0 +1,110 @@
+const pg = require("pg");
+const express = require("express");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_notes_32_ws"
+);
+const app = express();
+
+app.get("/api/notes", async (req, res, next) => {
+  try {
+    const SQL = `SELECT * FROM notes`;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/notes/:id", async (req, res, next) => {
+  try {
+    const SQL = `SELECT * FROM notes WHERE id=$1`;
+    const response = await client.query(SQL, [req.params.id]);
+    res.send(response.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/api/notes", async (req, res, next) => {
+  try {
+    const SQL = /* sql */ `
+          INSERT INTO notes(name, is_favorite)
+          VALUES($1, $2)
+          RETURNING *
+          `;
+    const response = await client.query(SQL, [
+      req.body.name,
+      req.body.is_favorite,
+    ]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.put("/api/notes/:id", async (req, res, next) => {
+  try {
+    const SQL = /* sql */ `
+          UPDATE notes
+          SET name=$1, is_favorite=$2, updated_at=now()
+          WHERE id=$3
+          RETURNING *
+          `;
+    const response = await client.query(SQL, [
+      req.body.name,
+      req.body.is_favorite,
+      req.params.id,
+    ]);
+    res.send(response.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/api/notes/:id", async (req, res, next) => {
+  try {
+    const SQL = /* sql */ `
+          DELETE from notes
+          WHERE id=$1
+          `;
+    await client.query(SQL, [req.params.id]);
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const init = async () => {
+  await client.connect();
+  console.log("connected to database");
+  let SQL =
+    /* sql */
+    `
+  DROP TABLE IF EXISTS notes;
+  CREATE TABLE notes (
+    
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
+    /* ranking INTEGER DEFAULT 3 NOT NULL, */
+    name VARCHAR(255),
+    is_favorite BOOLEAN DEFAULT FALSE
+  )
+  
+  `;
+  await client.query(SQL);
+  console.log("tables created");
+
+  SQL = /* sql */ `
+  INSERT INTO notes(name, is_favorite) VALUES('chocolate', true);
+  INSERT INTO notes(name, is_favorite) VALUES('pepper mint', false);
+    INSERT INTO notes(name, is_favorite) VALUES('strawberry', true);
+    INSERT INTO notes(name, is_favorite) VALUES('mango', true);
+  `;
+  await client.query(SQL);
+  console.log("data seeded");
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listen on port ${port}`));
+};
+
+init();
